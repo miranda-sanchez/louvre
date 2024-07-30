4f6bdaf35807c09948d5220e1ddf4ccfeb47fc06
miranda-sanchez <121776162+miranda-sanchez@users.noreply.github.com>
2024-07-30 16:11:48 -0300

    Changes in hero for it to be more responsive

--- src/pages/Exhibitions.js
+++ src/pages/Exhibitions.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ExhibitionsSection from "../components/ExhibitionsSection";
 import PageHeader from "../components/PageHeader";
 import headerImg from "../img/outside.jpg";
